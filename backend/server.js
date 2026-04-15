796232eaf29298f50e88ace57bb9b96f2023be9f
RicardoMA <148933348+RicardoMA23@users.noreply.github.com>
2026-04-15 00:16:21 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 5432');
+  console.log('Servidor corriendo en puerto 3000');
 });
