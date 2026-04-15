d0610e06b873592a598833995e51f22f074f0f6f
RicardoMA23 <rmontalvoacosta@gmail.com>
2026-04-15 00:14:29 -0600

    carpeta para el backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./routes');
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en puerto 5432');
+});
